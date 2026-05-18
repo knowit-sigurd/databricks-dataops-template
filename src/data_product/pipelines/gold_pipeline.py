12f2fa91d634e61eddaa41c64049884e02c6e62d
Sigurd Lislegaard <sigurd.lislegaard@knowit.no>
2026-05-18 11:59:08 +0000

    changes againt to pipelines

--- src/data_product/pipelines/gold_pipeline.py
+++ src/data_product/pipelines/gold_pipeline.py
@@ -1,7 +1,1 @@
-from pyspark import pipelines as dp
-from pyspark.sql.types import StructType
-
-
-@dp.materialized_view
-def customer_orders_gold():
-    return spark.createDataFrame([], StructType([]))
+from pyspark import pipelines as dp  # noqa: F401
--- src/data_product/pipelines/orders_pipeline.py
+++ src/data_product/pipelines/orders_pipeline.py
@@ -1,5 +1,4 @@
 from pyspark import pipelines as dp
-from pyspark.sql.types import StructType
 
 from data_product.domains.orders.transformations import BRONZE_SCHEMA
 
@@ -7,13 +6,3 @@ from data_product.domains.orders.transformations import BRONZE_SCHEMA
 @dp.table
 def orders_bronze():
     return spark.createDataFrame([], BRONZE_SCHEMA)
-
-
-@dp.table
-def orders_silver():
-    return spark.createDataFrame([], StructType([]))
-
-
-@dp.table
-def orders_rejected():
-    return spark.createDataFrame([], StructType([]))
